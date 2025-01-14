3ac85fdfb821c439c0ad5c1eaa191915003798d5
DarthNati <168640628+DarthNati@users.noreply.github.com>
2025-01-14 12:32:50 +0200

    Update teams and members

--- group.js
+++ group.js
@@ -1,109 +1,106 @@
 const COLORS = {
-  "Tier 2 IL": "#E49B0F",
-  "Tier 2 DUBAI": "#0047AB",
-  "Support Europe": "#9832a8",
-  "Support UK": "#32a877"
+    TIER_2_IL: '#E49B0F',
+    TIER_2_DUBAI: '#0047AB',
+    SUPPORT_UK: '#32a877',
+    SUPPORT_EU: '#9832a8'
 };
+
+// Team members configuration
 const TEAM_MEMBERS = {
-  "Nadav Arnheim": {
-    "group": "Tier 2 IL",
-    "color": "#E49B0F"
-  },
-  "Guy Kogan": {
-    "group": "Tier 2 IL",
-    "color": "#E49B0F"
-  },
-  "Galit Bezinian Ezov": {
-    "group": "Tier 2 IL",
-    "color": "#E49B0F"
-  },
-  "Hodaya Menahem": {
-    "group": "Tier 2 IL",
-    "color": "#E49B0F"
-  },
-  "Abizar Fakruddin": {
-    "group": "Tier 2 DUBAI",
-    "color": "#0047AB"
-  },
-  "Yasin Banu Shafi Mohamed": {
-    "group": "Tier 2 DUBAI",
-    "color": "#0047AB"
-  },
-  "Peeyush Sharma": {
-    "group": "Tier 2 DUBAI",
-    "color": "#0047AB"
-  },
-  "Erla Gudmundsdottir": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Stefan Orn Thorarinsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "María Ýr Valsdóttir": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Asdis Johannsdottir": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Gestur Helgason": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Stefán Hermundsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Arnar Freyr Sigurjonsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Bjorgvin Mar Gudjonsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Hrafnkell Sigurðarson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Adalsteinn Sigurgeirsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Muggur Ólafsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Bjartur Vidarsson": {
-    "group": "Support Europe",
-    "color": "#9832a8"
-  },
-  "Alexander Perry": {
-    "group": "Support UK",
-    "color": "#32a877"
-  },
-  "Connor Holland": {
-    "group": "Support UK",
-    "color": "#32a877"
-  },
-  "Bethany Wootton": {
-    "group": "Support UK",
-    "color": "#32a877"
-  },
-  "Yuen Fai Wan": {
-    "group": "Support UK",
-    "color": "#32a877"
-  },
-  "Sam Holloway": {
-    "group": "Support UK",
-    "color": "#32a877"
-  },
-  "Netanel Balas": {
-    "group": "Tier 2 IL",
-    "color": "#E49B0F"
-  }
+    'Nadav Arnheim': {
+        group: 'Tier 2 IL',
+        color: '#E49B0F'
+    },
+    'Guy Kogan': {
+        group: 'Tier 2 IL',
+        color: '#E49B0F'
+    },
+    'Galit Bezinian Ezov': {
+        group: 'Tier 2 IL',
+        color: '#E49B0F'
+    },
+    'Hodaya Menahem': {
+        group: 'Tier 2 IL',
+        color: '#E49B0F'
+    },
+    'Abizar Fakruddin': {
+        group: 'Tier 2 DUBAI',
+        color: '#0047AB'
+    },
+    'Yasin Banu Shafi Mohamed': {
+        group: 'Tier 2 DUBAI',
+        color: '#0047AB'
+    },
+    'Peeyush Sharma': {
+        group: 'Tier 2 DUBAI',
+        color: '#0047AB'
+    },
+    'Erla Gudmundsdottir': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Stefan Orn Thorarinsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'María Ýr Valsdóttir': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Asdis Johannsdottir': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Gestur Helgason': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Stefán Hermundsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Arnar Freyr Sigurjonsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Bjorgvin Mar Gudjonsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Hrafnkell Sigurðarson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Adalsteinn Sigurgeirsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Muggur Ólafsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Bjartur Vidarsson': {
+        group: 'Support Europe',
+        color: '#9832a8'
+    },
+    'Alexander Perry': {
+        group: 'Support UK',
+        color: '#32a877'
+    },
+    'Connor Holland': {
+        group: 'Support UK',
+        color: '#32a877'
+    },
+    'Bethany Wootton': {
+        group: 'Support UK',
+        color: '#32a877'
+    },
+    'Yuen Fai Wan': {
+        group: 'Support UK',
+        color: '#32a877'
+    },
+    'Sam Holloway': {
+        group: 'Support UK',
+        color: '#32a877'
+    }
 };
-export { COLORS, TEAM_MEMBERS };
