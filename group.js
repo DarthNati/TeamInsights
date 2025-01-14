d31885d2d125ca6f4ba88d9a52a80988e7d4f1a9
DarthNati <168640628+DarthNati@users.noreply.github.com>
2025-01-14 12:43:11 +0200

    Update group.js

--- group.js
+++ group.js
@@ -2,7 +2,8 @@ const COLORS = {
     TIER_2_IL: '#E49B0F',
     TIER_2_DUBAI: '#0047AB',
     SUPPORT_UK: '#32a877',
-    SUPPORT_EU: '#9832a8'
+    SUPPORT_EU: '#9832a8',
+    PARTNER_SUPPORT: '#6d6e70'
 };
 
 // Team members configuration
@@ -102,6 +103,10 @@ const TEAM_MEMBERS = {
     'Sam Holloway': {
         group: 'Support UK',
         color: '#32a877'
+    },
+        'Eden Elimelech': {
+        group: 'Partner Support',
+        color: '#6d6e70'
     },
     'Netanel Balas': {
         group: 'Tier 2 IL',
