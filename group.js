af1d8c61161fa18d7b67d10cceabdefdc60cdc73
DarthNati <168640628+DarthNati@users.noreply.github.com>
2025-01-14 12:30:43 +0200

    Update teams and members

--- group.js
+++ group.js
@@ -100,5 +100,10 @@ const TEAM_MEMBERS = {
   "Sam Holloway": {
     "group": "Support UK",
     "color": "#32a877"
+  },
+  "Netanel Balas": {
+    "group": "Tier 2 IL",
+    "color": "#E49B0F"
   }
 };
+export { COLORS, TEAM_MEMBERS };
