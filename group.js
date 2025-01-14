df77aa87454d9b53a4d70b9f867358526dd49d58
DarthNati <168640628+DarthNati@users.noreply.github.com>
2025-01-14 10:14:58 +0200

    Update teams and members

--- group.js
+++ group.js
@@ -2,7 +2,8 @@ const COLORS = {
   "TIER_2_IL": "#E49B0F",
   "TIER_2_DUBAI": "#0047AB",
   "SUPPORT_UK": "#32a877",
-  "SUPPORT_EU": "#9832a8"
+  "SUPPORT_EU": "#9832a8",
+  "test": "#ff0000"
 };
 const TEAM_MEMBERS = {};
 export { COLORS, TEAM_MEMBERS };
